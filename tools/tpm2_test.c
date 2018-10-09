7a12d03b68ed12135761af2dc95608f49f5a8dc6
Daniel Gracia Perez <daniel.gracia-perez@cfa-afti.fr>
2018-10-09 17:26:06 +0200

    Add tpm2 test program skeleton
    For the moment it's just a copy of the tpm2_createprimary, but will be
    rewritten to perform a complex test.

---
+++ tools/tpm2_test.c
@@ -0,0 +1,216 @@
+//**********************************************************************;
+// Copyright (c) 2015-2018, Intel Corporation
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and/or other materials provided with the distribution.
+//
+// 3. Neither the name of Intel Corporation nor the names of its contributors
+// may be used to endorse or promote products derived from this software without
+// specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+// THE POSSIBILITY OF SUCH DAMAGE.
+//**********************************************************************;
+
+#include <ctype.h>
+#include <limits.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <tss2/tss2_sys.h>
+
+#include "files.h"
+#include "log.h"
+#include "tpm2_alg_util.h"
+#include "tpm2_auth_util.h"
+#include "tpm2_attr_util.h"
+#include "tpm2_hierarchy.h"
+#include "tpm2_options.h"
+#include "tpm2_session.h"
+#include "tpm2_tool.h"
+#include "tpm2_util.h"
+
+#define DEFAULT_ATTRS \
+     TPMA_OBJECT_RESTRICTED|TPMA_OBJECT_DECRYPT \
+    |TPMA_OBJECT_FIXEDTPM|TPMA_OBJECT_FIXEDPARENT \
+    |TPMA_OBJECT_SENSITIVEDATAORIGIN|TPMA_OBJECT_USERWITHAUTH
+
+#define DEFAULT_PRIMARY_KEY_ALG "rsa2048:aes256"
+
+typedef struct tpm_protectprimary_ctx tpm_protectprimary_ctx;
+struct tpm_protectprimary_ctx {
+    struct {
+        TPMS_AUTH_COMMAND session_data;
+        tpm2_session *session;
+    } auth;
+    tpm2_hierarchy_pdata objdata;
+    char *context_file;
+    struct {
+        UINT8 P :1;
+        UINT8 p :1;
+    } flags;
+    char *parent_auth_str;
+    char *key_auth_str;
+
+    char *alg;
+    char *halg;
+    char *attrs;
+    char *policy;
+};
+
+static tpm_protectprimary_ctx ctx = {
+    .alg = DEFAULT_PRIMARY_KEY_ALG,
+    .auth = { .session_data = TPMS_AUTH_COMMAND_INIT(TPM2_RS_PW) },
+    .objdata = {
+        .in = {
+            .sensitive = TPM2B_SENSITIVE_CREATE_EMPTY_INIT,
+            .hierarchy = TPM2_RH_OWNER
+        },
+    },
+};
+
+static bool on_option(char key, char *value) {
+
+    bool res;
+
+    switch (key) {
+    case 'a':
+        res = tpm2_hierarchy_from_optarg(value, &ctx.objdata.in.hierarchy,
+                TPM2_HIERARCHY_FLAGS_ALL);
+        if (!res) {
+            return false;
+        }
+        break;
+    case 'P':
+        ctx.flags.P = 1;
+        ctx.parent_auth_str = value;
+        break;
+    case 'p':
+        ctx.flags.p = 1;
+        ctx.key_auth_str = value;
+        break;
+    case 'g':
+        ctx.halg = value;
+        break;
+    case 'G':
+        ctx.alg = value;
+        break;
+    case 'o':
+        ctx.context_file = value;
+        if (ctx.context_file == NULL || ctx.context_file[0] == '\0') {
+            return false;
+        }
+        break;
+    case 'L':
+        ctx.policy=value;
+    break;
+    case 'A':
+        ctx.attrs = value;
+    break;
+    /* no default */
+    }
+
+    return true;
+}
+
+bool tpm2_tool_onstart(tpm2_options **opts) {
+
+    const struct option topts[] = {
+        { "hierarchy",            required_argument, NULL, 'a' },
+        { "auth-hierarchy",       required_argument, NULL, 'P' },
+        { "auth-object",          required_argument, NULL, 'p' },
+        { "halg",                 required_argument, NULL, 'g' },
+        { "kalg",                 required_argument, NULL, 'G' },
+        { "out-context",          required_argument, NULL, 'o' },
+        { "policy-file",          required_argument, NULL, 'L' },
+        { "object-attributes",    required_argument, NULL, 'A' },
+    };
+
+    *opts = tpm2_options_new("A:P:p:g:G:o:L:a:", ARRAY_LEN(topts), topts,
+            on_option, NULL, 0);
+
+    return *opts != NULL;
+}
+
+int tpm2_tool_onrun(TSS2_SYS_CONTEXT *sapi_context, tpm2_option_flags flags) {
+    UNUSED(flags);
+
+    bool result;
+    int rc = 1;
+
+    if (ctx.flags.P) {
+        result = tpm2_auth_util_from_optarg(sapi_context, ctx.parent_auth_str, &ctx.auth.session_data, &ctx.auth.session);
+        if (!result) {
+            LOG_ERR("Invalid parent key authorization, got\"%s\"", ctx.parent_auth_str);
+            goto out;
+        }
+    }
+
+    if (ctx.flags.p) {
+        TPMS_AUTH_COMMAND tmp;
+        result = tpm2_auth_util_from_optarg(sapi_context, ctx.key_auth_str, &tmp, NULL);
+        if (!result) {
+            LOG_ERR("Invalid new key authorization, got\"%s\"", ctx.key_auth_str);
+            goto out;
+        }
+        ctx.objdata.in.sensitive.sensitive.userAuth = tmp.hmac;
+    }
+
+    result = tpm2_alg_util_public_init(ctx.alg, ctx.halg, ctx.attrs, ctx.policy, DEFAULT_ATTRS,
+            &ctx.objdata.in.public);
+    if(!result) {
+        goto out;
+    }
+
+    result = tpm2_hierarchy_create_primary(sapi_context, &ctx.auth.session_data, &ctx.objdata);
+    if (!result) {
+        goto out;
+    }
+
+    tpm2_util_public_to_yaml(&ctx.objdata.out.public, NULL);
+
+    tpm2_tool_output("handle: 0x%X\n", ctx.objdata.out.handle);
+
+    if (ctx.context_file) {
+        result = files_save_tpm_context_to_path(sapi_context, ctx.objdata.out.handle,
+            ctx.context_file);
+        if (!result) {
+            goto out;
+        }
+    }
+
+    rc = 0;
+
+out:
+    result = tpm2_session_save(sapi_context, ctx.auth.session, NULL);
+    if (!result) {
+        rc = 1;
+    }
+
+    return rc;
+}
+
+void tpm2_onexit(void) {
+
+    tpm2_session_free(&ctx.auth.session);
+}
